3f9cc6ce495eb82005b0d634a761843eb2f6830d
Maruti Rao Patkar <maruti.patkar45@gmail.com>
2026-08-20 23:26:24 +0530

    Time: 101 ms (63.93%), Space: 120.1 MB (39.24%) - LeetHub

--- 0575-distribute-candies/0575-distribute-candies.cpp
+++ 0575-distribute-candies/0575-distribute-candies.cpp
@@ -5,13 +5,10 @@
 class Solution {
 public:
     int distributeCandies(std::vector<int>& candyType) {
-        // Step 1: Count unique candy types using unordered_set
         std::unordered_set<int> uniqueCandies(candyType.begin(), candyType.end());
-        
-        // Step 2: Maximum candies Alice is allowed to eat
+
         int maxAllowed = candyType.size() / 2;
-        
-        // Step 3: Return the minimum of unique types and allowed capacity
+
         return std::min((int)uniqueCandies.size(), maxAllowed);
     }
 };
